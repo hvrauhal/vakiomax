c49637f8e8c0318b90d5c945e5f7e10c065b1d35
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2012-06-30 23:13:56 +0300

    This well go with

--- js/script.js
+++ js/script.js
@@ -27,27 +27,35 @@ $("#create-coupons").click(function (e) {
 	    return rows;
 	}
 	var rows = find_rows();
-    var coupon =  _.zip.apply(null, rows)
 
-    var coupon_contents = '<p>' + '<table id="coupon" class="un-submitted">';
-    coupon_contents += '<thead><tr><th></th>' + _.map(_.range(coupon[0].length), function (i) { return '<th>' + i + '.</th>'}).join('') + '</tr></thead>'
-    coupon_contents += _.map(_.zip(_.range(coupon.length), coupon),function (index_row) {
-        var index = index_row[0];
-        var row = index_row[1];
-        return '<tr>' +  '<th>' +  ++index  +
-            '</th>' + _.map(row,function (elem) {
-            return '<td>' + elem + '</td>';
-        }).join('') + '</tr>';
-    }).join('')
-
+    function add_a_coupon(rows) {
+        var coupon = _.zip.apply(null, rows)
+        var coupon_contents = '<p>' + '<table id="coupon" class="un-submitted">';
+        coupon_contents += '<thead><tr><th></th>' + _.map(_.range(coupon[0].length),function (i) {
+            return '<th>' + ++i + '.</th>'
+        }).join('') + '</tr></thead>'
+        coupon_contents += _.map(_.zip(_.range(coupon.length), coupon),function (index_row) {
+            var index = index_row[0];
+            var row = index_row[1];
+            return '<tr>' + '<th>' + ++index +
+                '. kohde</th>' + _.map(row,function (elem) {
+                return '<td>' + elem + '</td>';
+            }).join('') + '</tr>';
+        }).join('')
 //        '<tr><td>1.</td><td>x</td></tr>' +
-    coupon_contents +=
-        '</table>' +
-        '</p>';
-    var linktext = '<p><a class="btn btn-primary siirry" target="_blank" href="' + makeVeikkausUrl(rows) + '">Siirry maksamaan &raquo;</a></p>';
-    var new_coupon = '<div>' + coupon_contents + linktext + '</div>';
-    $("#coupons").append($(new_coupon).html());
-    })
+        coupon_contents +=
+            '</table>' +
+                '</p>';
+        var linktext = '<p><a class="btn btn-primary siirry" target="_blank" href="' + makeVeikkausUrl(rows) + '">Siirry maksamaan &raquo;</a></p>';
+        var new_coupon = '<div>' + coupon_contents + linktext + '</div>';
+        $("#coupons").append($(new_coupon).html());
+        $("#raw-rows").val("");
+    }
+    while (rows.length > 0) {
+        add_a_coupon(_.first(rows, 11));
+        rows = _.rest(rows, 11);
+    }
+})
 
 $('body').on('click', '.siirry', function (e) {
     $(this).removeClass('btn-primary');
